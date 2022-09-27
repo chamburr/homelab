36d03c7f7c916d1007dac110c84c5a922aebf286
Han Cen <hi@chamburr.com>
2022-09-27 18:31:13 +0800

    fix: use local token in vault

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -127,10 +127,7 @@ configureVault() {
       | vault policy write vault-secrets-operator -'
   kubectl -n vault exec vault-0 -- sh -c \
     'vault write auth/kubernetes/config \
-      issuer="https://kubernetes.default.svc.cluster.local" \
-      token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
-      kubernetes_host=https://${KUBERNETES_PORT_443_TCP_ADDR}:443 \
-      kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
+      kubernetes_host=https://${KUBERNETES_PORT_443_TCP_ADDR}:443'
   kubectl -n vault exec vault-0 -- sh -c \
     'vault write auth/kubernetes/role/vault-secrets-operator \
       bound_service_account_names=vault-secrets-operator \
