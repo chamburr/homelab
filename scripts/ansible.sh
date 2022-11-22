db8640b32e85f76a2a920ed2ee27ab9a7641fe2a
Han Cen <hi@chamburr.com>
2022-11-22 15:48:14 +0800

    refactor: restructure shell scripts

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
-user=$(grep USER= .env | cut -d '=' -f2-)
-password=$(grep PASSWORD= .env | cut -d '=' -f2-)
+prepare() {
+  user=$(grep ^USER= .env | cut -d '=' -f2-)
+  password=$(grep ^PASSWORD= .env | cut -d '=' -f2-)
 
-args='-i ansible/hosts.yml'
+  args='-i ansible/hosts.yml'
 
-if [ "$user" != '' ]; then
-  args="$args -u '$user'"
+  if [ "$user" != '' ]; then
+    args="$args -u '$user'"
+  fi
 
   if [ "$password" != '' ]; then
     args="$args -e 'ansible_password=$password'"
   fi
-fi
+}
 
-if [ "$1" = 'playbook' ]; then
-  shift 1
-  eval "ansible-playbook $args $@"
-else
-  eval "ansible $args $@"
-fi
+run() {
+  if [ "$1" = 'playbook' ]; then
+    shift 1
+    eval "ansible-playbook $args $@"
+  else
+    eval "ansible $args $@"
+  fi
+}
+
+prepare
+run
--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-export TF_VAR_CLOUDFLARE_EMAIL=$(grep CLOUDFLARE_EMAIL= .env | cut -d '=' -f2-)
-export TF_VAR_CLOUDFLARE_API_KEY=$(grep CLOUDFLARE_API_KEY= .env | cut -d '=' -f2-)
-export TF_VAR_CLOUDFLARE_ZONE_ID=$(grep CLOUDFLARE_ZONE_ID= .env | cut -d '=' -f2-)
-export TF_VAR_CLOUDFLARE_DOMAIN=$(grep CLOUDFLARE_DOMAIN= .env | cut -d '=' -f2-)
+prepare() {
+  export TF_VAR_CLOUDFLARE_EMAIL=$(grep CLOUDFLARE_EMAIL= .env | cut -d '=' -f2-)
+  export TF_VAR_CLOUDFLARE_API_KEY=$(grep CLOUDFLARE_API_KEY= .env | cut -d '=' -f2-)
+  export TF_VAR_CLOUDFLARE_ZONE_ID=$(grep CLOUDFLARE_ZONE_ID= .env | cut -d '=' -f2-)
+  export TF_VAR_CLOUDFLARE_DOMAIN=$(grep CLOUDFLARE_DOMAIN= .env | cut -d '=' -f2-)
+}
 
-terraform -chdir=terraform $@
+run() {
+  terraform -chdir=terraform $@
+}
+
+prepare
+run
