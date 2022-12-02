0f0e2c7075a42945ac6ae3485d3eb2df98c4751c
Han Cen <hi@chamburr.com>
2022-12-02 23:44:30 +0800

    chore: add output to bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
 
-deps() {
+prepare() {
   ansible-galaxy collection install -r ansible/requirements.yml
 }
 
-prepare() {
+install() {
+  echo "Installing..."
+
   ./scripts/ansible.sh playbook ansible/main.yml --tags common
 
   sed -i \
     -e "s/^USER=.*/USER=$(cat ansible/group_vars/all.yml | yq '.username')/" \
     -e "s/^PASSWORD=.*/PASSWORD=/" \
     .env
-}
 
-install() {
   export CONFIGURE_VARS=$(cat .env | grep '=' \
    | grep -v -e '^USER=' -e '^PASSWORD=' | sed 's/\(.*\)=/\L\1=/' | xargs)
 
   ./scripts/ansible.sh playbook ansible/main.yml --skip-tags common
 }
 
-deps
-prepare
 install
