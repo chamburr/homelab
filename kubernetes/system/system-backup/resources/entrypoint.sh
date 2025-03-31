0746102bb3e4bdd4ceb59a5d165c9c8010885047
Han Cen <hi@chamburr.com>
2025-03-31 21:14:20 +0800

    fix: reduce system backup interval

--- kubernetes/system/system-backup/resources/entrypoint.sh
+++ kubernetes/system/system-backup/resources/entrypoint.sh
@@ -3,6 +3,6 @@
 wget -O /usr/local/bin/talosctl https://github.com/siderolabs/talos/releases/latest/download/talosctl-linux-amd64
 chmod +x /usr/local/bin/talosctl
 
-echo "0 * * * * /bin/sh /app/cron.sh" > /var/spool/cron/crontabs/root
+echo "0 0 * * * /bin/sh /app/cron.sh" > /var/spool/cron/crontabs/root
 
 crond -f
